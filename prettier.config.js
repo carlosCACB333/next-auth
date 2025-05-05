0f001b79db9cff56310fe0a7b409866c9f9a82b6
Carlos <carloscacb333@gmail.com>
2025-05-05 18:20:20 -0500

    husky test6

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,15 @@
 const config = {
-	plugins: ['prettier-plugin-tailwindcss'],
-	useTabs: true,
-	semi: true,
-	singleQuote: true,
-	jsxSingleQuote: true,
-	trailingComma: 'all',
-	bracketSameLine: false,
-	arrowParens: 'always',
-	endOfLine: 'lf',
+  plugins: ['prettier-plugin-tailwindcss'],
+  trailingComma: 'es5',
+  singleQuote: true,
+  semi: true,
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  jsxSingleQuote: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'auto',
 };
 
 module.exports = config;
